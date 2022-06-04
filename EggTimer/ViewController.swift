6cb4090076c1d2e3c5e3fe98a5002144e5738cfd
Sachin Panayil <sachinpanayil01@gmail.com>
2022-06-03 20:33:53 -0400

    added an alarm when timer is done and finished rest of app

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,9 +7,58 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
-
-
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var progressView: UIProgressView!
+    
+    let eggTimes = ["Soft": 3, "Medium": 420, "Hard": 720]
+    var timer = Timer()
+    var player: AVAudioPlayer!
+    var totalTime = 0
+    var secondsPassed = 0
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+        timer.invalidate()
+        let hardness = sender.currentTitle!
+        totalTime = eggTimes[hardness]!
+        
+        progressView.progress = 0.0
+        secondsPassed = 0
+        titleLabel.text = hardness
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+        
+    }
+    
+    @objc func updateTimer() {
+        if secondsPassed < totalTime {
+            
+            secondsPassed += 1
+            var progressTime = Float(secondsPassed) / Float(totalTime)
+            progressView.progress = progressTime
+            
+            print(progressTime)
+            print("--------------------")
+            
+            if progressTime == 1.0 {
+                playSound()
+            }
+            
+        } else {
+            timer.invalidate()
+            titleLabel.text = "Done!"
+        }
+    }
+    
+    func playSound() {
+        let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+        
+    }
+    
 }
